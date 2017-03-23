5b2e5d71f74e7d4f0e0d6123c0de8d49f35db6fe
bafomdad <cheesecake@operamail.com>
2017-03-23 00:37:25 -0700

    version increase

--- com/bafomdad/realfilingcabinet/RealFilingCabinet.java
+++ com/bafomdad/realfilingcabinet/RealFilingCabinet.java
@@ -19,7 +19,7 @@ public class RealFilingCabinet {
 
 	public static final String MOD_ID = "realfilingcabinet";
 	public static final String MOD_NAME = "Real Filing Cabinet";
-	public static final String VERSION = "1.3.42";
+	public static final String VERSION = "1.3.5";
 	
 	@SidedProxy(clientSide="com.bafomdad.realfilingcabinet.proxies.ClientProxy", serverSide="com.bafomdad.realfilingcabinet.proxies.CommonProxy")
 	public static CommonProxy proxy;
